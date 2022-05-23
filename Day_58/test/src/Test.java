56e6ce27e67e4054d9bc4285a1e41ef390109478
Xavier Faulkner <xjfaulkner@gmail.com>
2022-05-23 08:47:12 -0400

    update

---
+++ Day_58/test/src/Test.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Test{
+	
+	public static void main(String[] args){
+		ArrayList<Integer> ints = new ArrayList<>();
+		ints.add(2);
+		System.out.println(ints);
+	}		
+}
